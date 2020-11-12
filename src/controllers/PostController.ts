12eb69ca342629630cafa2e0d51446bca557596f
HeitorFranco <heitorfranco04@gmail.com>
2020-11-11 21:21:00 -0300

    update

--- src/controllers/PostController.ts
+++ src/controllers/PostController.ts
@@ -5,11 +5,14 @@ import jwt from "jsonwebtoken";
 
 import User from "../models/User";
 import Post from "../models/Post";
+import userView from "../views/users_view";
+
+import postView from "../views/posts_view";
 
 export default {
   async index(req: Request, res: Response) {
     const postRepository = getRepository(Post);
-    const { page, limit } = req.query;
+    const { page = 1, limit = 0 } = req.query;
 
     const pageN = Number(page);
     const limitN = Number(limit);
@@ -19,7 +22,7 @@ export default {
     const posts = await postRepository.findAndCount({
       take: limitN,
       skip: offset,
-      relations: ["comments"],
+      relations: ["user", "comments"],
       order: { id: "ASC" },
     });
     {
@@ -28,30 +31,55 @@ export default {
     const total = posts[1];
     const pages = Math.ceil(total / limitN);
 
-    return res.json({
-      posts: [...posts[0]],
-      total,
-      pages,
-      limit: limitN,
-      page: pageN,
+    const dataUser = posts[0].map((post) => {
+      return userView.render(post.user);
     });
+
+    return res.json({
+      posts: postView.renderMany(posts[0]),
+      total,
+      pages: pages ? pages : undefined,
+      limit: limit ? limitN : undefined,
+      page: pageN ? pageN : undefined,
+    });
+  },
+  async show(req: Request, res: Response) {
+    const postRepository = getRepository(Post);
+    const { id } = req.params;
+
+    const post = await postRepository.findOneOrFail({
+      where: { id },
+      relations: ["comments", "user"],
+    });
+
+    return res.json(postView.render(post));
   },
 
   async create(req: Request, res: Response) {
+    const { description, likes } = req.body;
+
     const postRepository = getRepository(Post);
     const userRepository = getRepository(User);
 
     const user = await userRepository.findOne({ where: { id: req.userId } });
 
+    const requestImages = req.file;
+    const photo_path = requestImages.filename;
+
     const post = postRepository.create({
-      ...req.body,
+      description,
+      likes,
+      photo_path,
       user: user,
     });
 
     delete post.user.password;
-
     await postRepository.save(post);
+    delete post.photo_path;
 
-    return res.json(post);
+    return res.json({
+      post,
+      url: `http://localhost:3333/uploads/${photo_path}`,
+    });
   },
 };
--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -4,24 +4,30 @@ import jwt from "jsonwebtoken";
 
 import User from "../models/User";
 
+import userView from "../views/users_view";
+
 export default {
   async index(req: Request, res: Response) {
     const repository = getRepository(User);
-    const user = await repository.findOne({
+    const user = await repository.findOneOrFail({
       where: { id: req.userId },
       relations: ["posts"],
     });
 
-    return res.json({ user });
+    return res.json(userView.render(user));
   },
   async create(req: Request, res: Response) {
     const { name, email, password } = req.body;
     const repository = getRepository(User);
 
+    const requestImages = req.file;
+    const photo_path = requestImages.filename;
+
     if (await repository.findOne({ where: { email } }))
       return res.status(409).send("Usuário já cadastrado no sistema");
 
-    const user = repository.create({ name, email, password });
+    const user = repository.create({ name, email, password, photo_path });
+    console.log(user);
     await repository.save(user);
 
     const token = jwt.sign({ id: user.id }, "secret", { expiresIn: "1d" });
