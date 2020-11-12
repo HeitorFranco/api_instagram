5c9774cb5b47f793942befa55e8044cd30178bf6
HeitorFranco <heitorfranco04@gmail.com>
2020-11-11 21:18:51 -0300

    add post_view

---
+++ src/views/posts_view.ts
@@ -0,0 +1,28 @@
+import Post from "../models/Post";
+
+import userView from "./users_view";
+
+export default {
+  render(post: Post) {
+    delete post.user.password;
+    if (post.comments) {
+      post.comments.forEach((post) => {
+        delete post.user.password;
+      });
+    }
+    return {
+      id: post.id,
+      description: post.description,
+      likes: post.likes,
+      user: userView.render(post.user),
+      comments: post.comments,
+      url: `http://localhost:3333/uploads/${post.photo_path}`,
+    };
+  },
+  renderMany(posts: Post[]) {
+    return posts.map((post) => {
+      post.user.password = undefined;
+      return this.render(post);
+    });
+  },
+};
