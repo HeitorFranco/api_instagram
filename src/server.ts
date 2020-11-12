8c8c746ed4811f0d90064184aa2f571627083c3f
HeitorFranco <heitorfranco04@gmail.com>
2020-11-11 21:21:59 -0300

    adding cors and static upload route

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,14 @@
 import express from "express";
 import routes from "./routes";
+import path from "path";
+import cors from "cors";
 
 const app = express();
 import "./database/connect";
 
+app.use(cors());
 app.use(express.json());
+app.use("/uploads", express.static(path.join(__dirname, "..", "uploads")));
 app.use(routes);
 
 app.listen(3333, () => console.log("🏃 Running server"));
